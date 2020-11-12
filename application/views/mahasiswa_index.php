7513395cb7985e032e801a34800f287b152ff50e
dikaptmm <dikaprtm48@gmail.com>
2020-11-12 12:24:25 +0800

    update bootsstrap

--- application/views/mahasiswa_index.php
+++ application/views/mahasiswa_index.php
@@ -2,20 +2,53 @@
 <html>
 <head>
 	<title>Mahasiswa Index</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
 </head>
 <body>
-
+	<nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #7A77FF">
+		<a href="" class="navbar-brand">
+			Tugas
+		</a>
+		<div class="navbar-nav ml-auto">
+			<a href="" class="nav-link active">Daiki</a>
+		</div>
+	</nav>
+	<div class="alert alert-success text-center mt-2 shadow">
+		Data Mahasiswa
+	</div>
+	<div class="container">
+		<table class="table table-bordered table-striped table-hover">
+			<tr class="text-center">
+				<th>NO</th>
+				<th>NIM</th>
+				<th>Nama</th>
+				<th>Alamat</th>
+				<th>AKsi</th>
+			</tr>
+			<tr>
+				<th>1</th>
+				<th>1810330026</th>
+				<th>Dikaw</th>
+				<th>Mataram</th>
+				<td class="text-center">
+					<a href="" class="btn btn-info btn-sm">Edit</a>
+					<a href="" class="btn btn-danger btn-sm">Edit</a>
+				</td>
+			</tr>
+		</table>
+	</div>
 	<h1>Mahasiswa</h1>
 	<h3>Selamat Datang, <?= $nama_lengkap ?></h3>
 	<p>Nim saya <?= $nim ?></p>
 	<hr>
-	<a href="<?= site_url('mahasiswa/tambah')?>"></a>
+	<a href="<?= site_url('Mahasiswa')?>">Kembali</a>
+	<a href="<?= site_url('Mahasiswa/tambah') ?>">Tambah Mahasiswa</a>
 	<table border="1">
 		<tr>
 			<th>Nim</th>
 			<th>Nama Mahasiswa</th>
 		</tr>
-		<?php foreach ($mahasis as $key): ?>
+		<?php foreach ($mahasiswa as $key): ?>
 			<tr>
 				<td><?= $key['nim'] ?></td>
 				<td><?= $key['nama'] ?></td>
--- application/views/mahasiswa_tambah.php
+++ application/views/mahasiswa_tambah.php
@@ -2,9 +2,53 @@
 <html>
 <head>
 	<title>Mahasiswa</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
 </head>
 <body>
-	<h1>Tmabah Mahasiswa</h1>
+	<nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #7A77FF">
+		<a href="" class="navbar-brand">
+			Tugas
+		</a>
+		<div class="navbar-nav">
+			<a href="<?= site_url('Mahasiswa')?>">Home</a>
+			<a href="<?= site_url('mahasiswa_tambah')?>" class="nav-link active">Tambah Data</a>
+		</div>
+		<div class="navbar-nav ml-auto">
+			<a href="" class="nav-link active">Daiki</a>
+		</div>
+	</nav>
+	<div class="alert alert-success text-center mt-2 shadow">
+		Tambah Mahasiswa
+	</div>
+	<br><br>
+	<div>
+		<div class="card mt-2">
+			<div class="card-body">
+				<div class="from-group row">
+					<label for="inputNIM" class="col-sm-2 col-from-label">NIM</label>
+					<div class="com-sm-10">
+						<input type="text" class="from-control" id="NIM" placeholder="Masukan NIM">
+					</div>
+				</div>
+				<div class="from-group row">
+					<label for="inputNama" class="col-sm-2 col-from-label">Nama</label>
+					<input type="text" class="from-control" id="Nama" placeholder="Masukan Nama">
+
+				</div>
+				<div class="from-group row">
+					<label for="inputAlamat" class="col-sm-2 col-from-label">Alamat</label>
+					<div class="col-md-10">
+						<textarea name="Alamat" class="from-control" placeholder="Masukan Alamat"></textarea>
+					</div>
+				</div>
+				<div align="right">
+					<input type="submit" name="btnsubmit" value="simpan" class="btn btn-success mt-1">
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<h1>Tambah Mahasiswa</h1>
 	<a href="">Kembali</a>
 	<br>
 	<form>
